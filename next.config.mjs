e64e028ec5b7286e742eceb5bfe9d7e04ae326f5
dongjun1217 <ehdwns9294@naver.com>
2024-06-12 15:03:20 +0900

    next.config 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
