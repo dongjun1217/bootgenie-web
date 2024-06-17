cc21bcd616d2a1ea5f601b1ca7d3acbad0d0ec30
dongjun1217 <ehdwns9294@naver.com>
2024-06-17 19:11:46 +0900

    https -> http 롤백

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     
     <html lang="en" className="dark">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
