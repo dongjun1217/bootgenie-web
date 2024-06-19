e973d2c9daf5b8fb7c3038f11e7238b954fe8a16
dongjun1217 <ehdwns9294@naver.com>
2024-06-19 17:26:43 +0900

    빌드 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,15 @@ const withMDX = createMDX();
 const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-    },
+          // !! WARN !!
+          // Dangerously allow production builds to successfully complete even if
+          // your project has type errors.
+          // !! WARN !!
+          ignoreBuildErrors: true,
+  },
 };
 
 export default withMDX(nextConfig);
