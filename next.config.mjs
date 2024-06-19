4c1388041f43841832cedc214ee8e4adb67442c8
dongjun1217 <ehdwns9294@naver.com>
2024-06-19 17:33:35 +0900

    빌드 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 import createMDX from 'fumadocs-mdx/config';
 
-const withMDX = createMDX();
+const withMDX = createMDX({
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  }
+});
 
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  typescript: {
-          // !! WARN !!
-          // Dangerously allow production builds to successfully complete even if
-          // your project has type errors.
-          // !! WARN !!
-          ignoreBuildErrors: true,
-  },
 };
 
-export default nextConfig;
+export default createMDX(nextConfig);
