6ca5fb21c69c9a3c446dac96f51708be78fb5f15
dongjun1217 <ehdwns9294@naver.com>
2024-06-19 18:07:39 +0900

    빌드 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -7,19 +7,6 @@ const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/bootgenie-web', // <repository-name>을 실제 리포지토리 이름으로 대체
-  assetPrefix: '/bootgenie-web/',
-  images: {
-    unoptimized: true,
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' }
-    };
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
