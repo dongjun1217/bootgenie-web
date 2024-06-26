b5259ac49ec330192d6b3a2e1f712a5ab35ce767
dongjun1217 <ehdwns9294@naver.com>
2024-06-26 23:27:43 +0900

    speed insights 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import type { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function Layout({
         <body className={inter.className}>
           {children}
           <Analytics/>
+          <SpeedInsights />
         </body>
     </html>
   );
